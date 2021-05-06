a3bb30300d9ba40e32c557cb5ef1b725b2919660
chalkedpalms <83742588+chalkedpalms@users.noreply.github.com>
2021-05-06 01:54:40 -0400

    updated greeting.js messages to french
    I delete "gree 6" and add a new else if condition for hour 15-18

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,24 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Michael';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const gree1 = 'Bonsoir!  ';
+const gree2 = 'Bonjour!  ';
+const gree3 = 'Salut  ';
+const gree4 = 'Ciao,  ';
+const gree5 = 'Bonsoir,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
   document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
-} else if (hour >= 12 && hour < 17) {
+} else if (hour >= 12 && hour < 15) {
   document.getElementById('greetings').innerText = gree3 + name;
-} else {
+} else if (hour >= 15 && hour < 18) {
   document.getElementById('greetings').innerText = gree4 + name;
+  else {
+  document.getElementById('greetings').innerText = gree5 + name;
 }
